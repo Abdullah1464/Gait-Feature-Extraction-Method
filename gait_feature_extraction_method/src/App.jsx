48c405f6cfb05dec9a30c297fb5b85f00af2ab8a
Abdullah1464 <muhammad.abdullah2gr8@gmail.com>
2024-03-06 16:19:01 +0500

    init 2

--- gait_feature_extraction_method/src/App.jsx
+++ gait_feature_extraction_method/src/App.jsx
@@ -3,20 +3,21 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import ButtonAppBar from './appbar';
 
-function ButtonAppBar() {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar >
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign:"center" }}>
-            News
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
-}
+// function ButtonAppBar() {
+//   return (
+//     <Box sx={{ flexGrow: 1 }}>
+//       <AppBar >
+//         <Toolbar>
+//           <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign:"center" }}>
+//             News
+//           </Typography>
+//         </Toolbar>
+//       </AppBar>
+//     </Box>
+//   );
+// }
 
 
 function App() {
