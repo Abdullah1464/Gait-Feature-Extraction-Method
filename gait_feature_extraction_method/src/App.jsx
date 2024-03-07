4112422f5a3a0a4bda45b0bdc7ca2df37ccecb9a
Abdullah1464 <muhammad.abdullah2gr8@gmail.com>
2024-03-07 16:51:17 +0500

    Dockerized the app

--- gait_feature_extraction_method/src/App.jsx
+++ gait_feature_extraction_method/src/App.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -9,7 +9,16 @@ import methodologyGif from './Proposed Methodology.gif';
 import results from './resultstable.png'; 
 import DenseTable from './createtable';
 import ResponsiveBarChart from './MetricsTable';
+import Button from '@mui/material/Button';
+import Heterogenous from './Heterogenous Experiment.png'
 function App() {
+
+  const [showTypewriter, setShowTypewriter] = useState(true);
+
+  const handleClick = () => {
+    setShowTypewriter(false);
+    setTimeout(() => setShowTypewriter(true), 0);
+  };
 
   return (
     <>
@@ -20,7 +29,7 @@ function App() {
             Introduction
           </Typography>
             <Typography variant="h4" sx={{ textAlign:"justify" ,position: 'relative', top: '35%', margin: '0 2rem' }}>
-              <Typewriter text={"Gait recognition refers to the identification of individuals based on features acquired from their body movement during walking. Despite the recent advances in the feature extraction process, variations in things such as camera angles, subject pose, occlusions, and clothing make it a computationally challenging task to extract an optimal set of features from gait data. Our research aims to contribute to the state-of-the-art with a machine learning framework for extracting robust gait features from gait data that both reduces the dimensionality of the gait data and maximizes the class separability. This will help us to learn what aspects of walking people generally differ and extract those as general gait features, improving the discriminative power of our feature set. To identify people without the need for group-specific features is convenient as particular people might not always provide annotated learning data. Our research aims to explore Mocap datasets especially as they remove the need for preprocessing, allowing us to focus solely on optimizing the feature extraction process."} delay={20} />
+              <Typewriter text={"Gait recognition refers to the identification of individuals based on features acquired from their body movement during walking. Despite the recent advances in the feature extraction process, variations in things such as camera angles, subject pose, occlusions, and clothing make it a computationally challenging task to extract an optimal set of features from gait data. Our research aims to contribute to the state-of-the-art with a machine learning framework for extracting robust gait features from gait data that both reduces the dimensionality of the gait data and maximizes the class separability. This will help us to learn what aspects of walking people generally differ and extract those as general gait features, improving the discriminative power of our feature set. To identify people without the need for group-specific features is convenient as particular people might not always provide annotated learning data. Our research aims to explore Mocap datasets especially as they remove the need for preprocessing, allowing us to focus solely on optimizing the feature extraction process."} delay={10} />
             </Typography>
         </Box>
 
@@ -42,14 +51,40 @@ function App() {
 
       <Box sx={{ scrollSnapAlign: 'start', height: '100vh' , bgcolor: 'beige'}}>
         <Typography variant="h3" component="h3" gutterBottom sx={{ textAlign:"center", position: 'relative', top: '20%', color: '#FE6B8B' }}>
-          Class Seperability Coefficients and Classification Metrics Comparison
+          Class Separability Coefficients and Classification Metrics Comparison
         </Typography>
         <Typography variant="h5" component="h5" gutterBottom sx={{ textAlign:"center", position: 'relative', top: '20%', color: 'black' }}>
-          Choose Comparison Metrics from the drop down below: 
+          Choose from the drop down below: 
         </Typography>
         <Box sx={{ mt: 3, mx: 4 ,position: 'relative', top: '19%'}}>
         <ResponsiveBarChart />
         </Box>
+      </Box>
+
+      <Box sx={{ scrollSnapAlign: 'start', height: '100vh' , bgcolor: 'white'}}>
+        <Typography variant="h3" component="h3" gutterBottom sx={{ textAlign:"center", position: 'relative', top: '20%', color: '#FE6B8B' }}>
+          Heterogeneous Metric 
+        </Typography>
+        <Box sx={{ mt: 5, mx: 5 ,position: 'relative', top: '20%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <img src={Heterogenous}  style={{height: '50%', width: '50%'}}/>
+        </Box>
+        <Typography variant="h5" component="h5" gutterBottom sx={{ textAlign:"center", position: 'relative', top: '20%', color: 'black' }}>
+          Experiment: Increasing the subjects increased the metrics score as more identities are used for training
+        </Typography>
+      </Box>
+
+      <Box sx={{ scrollSnapAlign: 'start', height: '100vh' }}>
+        <Typography variant="h2" component="h2" gutterBottom sx={{ textAlign:"center", position: 'relative', top: '20%', color: '#FE6B8B' }}>
+          Conclusion
+        </Typography>
+        <Button size='small' variant="contained" color="primary" onClick={handleClick} sx={{fontSize: '0.8rem', padding: '0.5rem 1rem', borderRadius: '20px', textTransform: 'none' }}>
+          Animate
+        </Button>
+        {showTypewriter && (
+          <Typography variant="h4" sx={{ textAlign:"justify" ,position: 'relative', top: '20%', margin: '0 2rem' }}>
+            <Typewriter text={"In the past, gait recognition features were extracted manually by hand. The field has not advanced much to the point where machine learning could be used to extract these features. Utilizing deep learning models increases the complexity of the models and thus increases the computational time. Although they are accurate but cannot be used in practical terms due to their huge time and space complexity. Our research introduces a balanced approach for gait recognition on unknown subjects for which the identity is not known but the samples are available. It is done by modification of fisher linear discriminant analysis with maximum margin criterion. It is observed that this method outperforms other hand-crafted methods. Although PCA/LDA is performing better in terms of CCR score, but MMC model is also very close to it when it comes to accuracy with far less computational time. The MMC method performs well when evaluated on 4 class separability coefficients, making it very useful in gait recognition applications. The system is tested on 27 walkers and then evaluated on 27 walkers’ making it 50\% ratio with an accuracy of 95.3\% in terms of CCR and when tested on 14 walkers and evaluated on 40 walkers making it 25\% training and 75\% testing, it was over 90\% which depicts that greater the number of identities given for training, better is the result for evaluation."} delay={10} />
+          </Typography>
+        )}
       </Box>
       
     </>
--- gait_feature_extraction_method/src/createtable.jsx
+++ gait_feature_extraction_method/src/createtable.jsx
@@ -11,16 +11,16 @@ function createData(Method,CCR,EER,AUC,MAP,DCT,TD,DBI,DI,SC,FDR,Time) {
 }
 
 const rows = [
-  createData('MMC Transform',0.9532,0.3945,0.7273,0.3348,0.0048,26,2.3493,0.1652,0.0177,1.1266,'57.3 m'),
-  createData('PCA/LDA Transform',0.9607,0.4389,0.7098,0.3384,0.0038,27,2.8163,0.1417,-0.0263,1.0162,'1.8 h'),
-  createData('Mohammad Ahmed',0.0063,0.6158,0.1975,0.4391,24,2.9642,0.1321,-0.1836,1.0099,0.4261,'59 m'),
-  createData('Adrian Ball',0.8563,0.4013,0.6483,0.2456,0.0026,18,2.8266,0.1639,-0.1346,1.1241,'45 m'),
-  createData('Bojan Divoski',0.8563,0.4007,0.6491,0.2757,0.0069,71,1.9502,0.2322,-0.0576,1.3837,'47 m'),
-  createData('KwolekB',0.9438,0.413,0.6845,0.3016,0.06,660,2.2992,0.1778,-0.0749,1.1493,'1.1 h'),
-  createData('PreisJ',0.9434,0.4461,0.5677,0.0965,0.0017,13,12.5721,0.0216,-0.4941,1.0754,'49 m'),
-  createData('SinhaA',0.2092,0.4153,0.6224,0.1918,0.0048,45,2.7097,0.181,-0.2226,1.1492,'51 m'),
-  createData('AliS',0.8033,0.4535,0.6108,0.1321,9.00E-04,2,7.0848,0.0144,-0.4206,1.2709,'41 m'),
-  createData('AnderssonVO',0.2968,0.3774,0.6722,0.2636,0.0068,68,2.3017,0.1986,-0.0752,1.1362,'45 m'),
+  createData('MMC Transform, Our Approach',0.9532,0.3945,0.7273,0.3348,0.0048,26,2.3493,0.1652,0.0177,1.1266,57.3),
+  createData('PCA/LDA Transform, Base Paper',0.9607,0.4389,0.7098,0.3384,0.0038,27,2.8163,0.1417,-0.0263,1.0162,108),
+  createData('Mohammad Ahmed',0.0063,0.6158,0.1975,0.4391,24,2.9642,0.1321,-0.1836,1.0099,0.4261,59),
+  createData('Adrian Ball',0.8563,0.4013,0.6483,0.2456,0.0026,18,2.8266,0.1639,-0.1346,1.1241,45),
+  createData('Bojan Divoski',0.8563,0.4007,0.6491,0.2757,0.0069,71,1.9502,0.2322,-0.0576,1.3837,47),
+  createData('KwolekB',0.9438,0.413,0.6845,0.3016,0.06,660,2.2992,0.1778,-0.0749,1.1493,66),
+  createData('PreisJ',0.9434,0.4461,0.5677,0.0965,0.0017,13,12.5721,0.0216,-0.4941,1.0754,49),
+  createData('SinhaA',0.2092,0.4153,0.6224,0.1918,0.0048,45,2.7097,0.181,-0.2226,1.1492,51),
+  createData('AliS',0.8033,0.4535,0.6108,0.1321,9.00E-04,2,7.0848,0.0144,-0.4206,1.2709,41),
+  createData('AnderssonVO',0.2968,0.3774,0.6722,0.2636,0.0068,68,2.3017,0.1986,-0.0752,1.1362,45),
 ];
 
 export default function DenseTable() {
@@ -44,10 +44,15 @@ export default function DenseTable() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => (
-              <TableRow key={row.Method} sx={{ color: 'white' }}>
-                <TableCell component="th" scope="row" sx={{ color: 'white' }}>
-                  {row.Method}
+            {rows.map((row, index) => (
+                <TableRow 
+                key={row.Method} 
+                sx={{ color: 'white', fontWeight: index === 0 ? 'bold' : 'normal', '&:hover': {
+                    backgroundColor: 'rgba(255, 255, 255, 0.1)',
+                  }, }}
+                >
+                <TableCell component="th" scope="row" sx={{ color: index === 0 ? 'red' : 'white' }}>
+                    {row.Method}
                 </TableCell>
                 <TableCell align="right" sx={{ color: 'white' }}>{row.CCR}</TableCell>
                 <TableCell align="right" sx={{ color: 'white' }}>{row.EER}</TableCell>
@@ -60,7 +65,7 @@ export default function DenseTable() {
                 <TableCell align="right" sx={{ color: 'white' }}>{row.SC}</TableCell>
                 <TableCell align="right" sx={{ color: 'white' }}>{row.FDR}</TableCell>
                 <TableCell align="right" sx={{ color: 'white' }}>{row.Time}</TableCell>
-              </TableRow>
+                </TableRow>
             ))}
           </TableBody>
         </Table>
